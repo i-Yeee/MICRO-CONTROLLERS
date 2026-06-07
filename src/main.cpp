c6395f781a173b53f747c23c033deab1471249f0
i-Yeee <josephkiokoseko@gmail.com>
2026-06-07 21:22:41 +0300

    blinking the onboard rgb led

---
+++ src/main.cpp
@@ -0,0 +1,34 @@
+#include <Arduino.h>
+
+// put function declarations here:
+#include <Adafruit_NeoPixel.h>
+
+Adafruit_NeoPixel LED_RGB(1, 48, NEO_GRB + NEO_KHZ800);
+
+void setup() {
+  LED_RGB.begin();
+  LED_RGB.setBrightness(20);
+}
+
+void loop() {
+  LED_RGB.setPixelColor(0, uint32_t(LED_RGB.Color(255, 0, 0)));
+  LED_RGB.show();
+  delay(2000);
+  LED_RGB.setPixelColor(0, uint32_t(LED_RGB.Color(0, 255, 0)));
+  LED_RGB.show();
+  delay(2000);
+
+  LED_RGB.setPixelColor(0, uint32_t(LED_RGB.Color(0, 0, 255)));
+  LED_RGB.show();
+  delay(2000);
+  LED_RGB.setPixelColor(0, uint32_t(LED_RGB.Color(255, 0, 255)));
+  LED_RGB.show();
+  delay(2000);
+  LED_RGB.setPixelColor(0, uint32_t(LED_RGB.Color(255, 255, 0)));
+  LED_RGB.show();
+  delay(2000);
+
+LED_RGB.setPixelColor(0, uint32_t(LED_RGB.Color(0, 0, 0)));
+  LED_RGB.show();
+  delay(2000);
+}
